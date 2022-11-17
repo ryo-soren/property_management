0b48e988ee7337915c1adc7dd6cc8400e6186b62
ryoS <ryosorensen0606@gmail.com>
2022-11-17 12:20:15 -0800

    routing issues fixed in questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   resource :session, only:[:new, :create, :destroy]
 
-  resources :users, except: [:edit, :update, :destroy] do
+  resources :users, only: [:new, :create] do
     get "admin_panel", to: "users#admin_panel"
   end
   
   resources :properties do
-    resources :questions do
-      resources :answers do
+    resources :questions, only:[:create, :destroy] do
+      resources :answers, only: [:create] do
       end
     end
   end
