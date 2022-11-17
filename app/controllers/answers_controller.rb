98f52c8966ee059b872f3aa366f4f81751e62366
Jason Fu <jason.fz@hotmail.com>
2022-11-17 09:08:25 -0800

    qestion/answer controller

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,50 @@
+class AnswersController < ApplicationController
+
+    before_action :find_question
+    before_action :authenticate_user!
+    # before_action :authorize_user!, only: [:destroy]
+  
+    def create
+      @answer = Answer.new(answer_params)
+      @answer.question = @question
+      @answer.user = current_user
+      if @answer.save
+    
+        flash[:success] = "Answer successfully created"
+        redirect_to @question
+      else
+       
+        @answers = @question.answers.order(created_at: :desc)
+
+        render '/questions/show', status: 303
+      end
+    end
+  
+   
+    def destroy
+      @answer = Answer.find params[:id]
+    #   if can?(:crud, @answer)
+        @answer.destroy
+        redirect_to question_path(@answer.question)
+        flash[:success] = "Answer deleted"
+    #   else
+    #     redirect_to root_path, alert: "Not athorized to change Answer!"
+    #   end
+    end
+    
+    private
+  
+    def find_question
+      @question = Question.find params[:question_id]
+    end
+   
+    def answer_params
+      params.require(:answer).permit(:body)
+    end
+  
+    # def authorize_user!
+    #   redirect_to root_path, alert: "Not authorized!" unless can?(:crud, @answer)
+    # end
+  
+  end
+  
