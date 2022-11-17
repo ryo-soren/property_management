b33ca2cb278a75f6c04110fff7e3c87da6922308
ryoS <ryosorensen0606@gmail.com>
2022-11-16 21:25:23 -0800

    Added 'create' methods to the users/sessions controllers as well as 'destroy' method to the sessions controller. Tested the functionality of all the methods with rough views for properties#index/root, sessions#new, users#new. Added validations to the user model for f.name, l.name, password, email. Added authorization methods using 'cancan' in path: app/models/ability.rb. Also added global methods to application controller for current user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by_id session[:user_id]
+    end
+
+    helper_method :current_user
+
+    def is_admin?
+        current_user.admin? == true
+    end
+
+    helper_method :is_admin?
+
+    def user_signed_in?
+        current_user.present?
+    end
+
+    helper_method :user_signed_in?
+
+    def authenticate_user!
+        redirect_to new_session_path, notice: "Please sign in" unless user_signed_in?
+    end
+
 end
