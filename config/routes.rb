da16d12c9708b8cd5d9fd4ada50985c8bd58854b
ryoS <ryosorensen0606@gmail.com>
2022-11-17 16:59:48 -0800

    added application model, applications controller, and seed file to include applications

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
 
   resource :session, only:[:new, :create, :destroy]
 
-  resources :users, only: [:new, :create] do
-    
-  end
-  get "admin_panel", to: "users#admin_panel"
+  resources :users, only: [:new, :create]
+
+  get "admin_panel", to: "users#admin_panel" 
   get "customer_panel", to: "users#customer_panel"
 
   resources :properties do
+    resources :applications
     resources :questions, only:[:create, :destroy] do
       resources :answers, only: [:create] do
       end
     end
+
   end
 end
